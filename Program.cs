8536748c924c302ac44aa9c80c69c6c01ee81e4f
KasparSiivelt5 <89830554+KasparSiivelt5@users.noreply.github.com>
2023-05-17 15:02:13 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Valige meetod (1, 2 või 3):");
+        int valik = int.Parse(Console.ReadLine());
+
+        if (valik == 1)
+        {
+            TeeFailDesktopile();
+        }
+        else if (valik == 2)
+        {
+            LoobFailiSisuga();
+        }
+        else if (valik == 3)
+        {
+            LoobFailiFailiUrliga();
+        }
+        else
+        {
+            Console.WriteLine("Vigane valik!");
+        }
+    }
+
+    static void TeeFailDesktopile()
+    {
+        string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        string failiNimi = "fail.txt";
+        string failiTee = Path.Combine(desktopPath, failiNimi);
+
+        File.Create(failiTee);
+        Console.WriteLine("Fail loodud desktopile: " + failiTee);
+    }
+
+    static void LoobFailiSisuga()
+    {
+        string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        string failiNimi = "fail.txt";
+        string failiTee = Path.Combine(desktopPath, failiNimi);
+
+        string sisu = "See on faili sisu, mille olete määranud.";
+        File.WriteAllText(failiTee, sisu);
+
+        Console.WriteLine("Fail loodud desktopile sisuga: " + failiTee);
+    }
+
+    static void LoobFailiFailiUrliga()
+    {
+        string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        string failiNimi = "fail.txt";
+        string failiTee = Path.Combine(desktopPath, failiNimi);
+
+        string url = "https://www.minuurl.com/fail.txt";
+        File.WriteAllText(failiTee, url);
+
+        Console.WriteLine("Fail loodud desktopile URL-iga: " + failiTee);
+    }
+}
